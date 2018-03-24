7f56f7ce6fc7afb7e6fb729d0b8022b53073e397
Unknown <oreoboi@gmail.com>
2018-03-24 01:29:16 -0400

    Worked on schema

--- src/models/file.models.js
+++ src/models/file.models.js
@@ -1,29 +1,64 @@
 // Load mongoose package
 const mongoose = require('mongoose');
 
-const FileSchema = new mongoose.Schema({
+// Image schema
+var schema = new Schema({
+  img: { data: Buffer, contentType: String },
   title: String,
   description: String,
   created_at: { type: Date, default: Date.now },
+  deleted: {type: Boolean, default: false},
 });
 
-const File = mongoose.model('File', FileSchema);
+// our model
+var Cat = mongoose.model('Cat', schema);
 
-File.count({}, function(err, count) {
-  if (err) {
-    throw err;
-  }
-  
-  if (count > 0) return ;
+mongoose.connection.on('open', function () {
+console.error('mongo is open');
 
-  const files = require('./file.seed.json');
-  File.create(files, function(err, newFiles) {
-    if (err) {
-      throw err;
-    }
-    console.log("DB seeded")
+// empty the collection
+Cat.remove(function (err) {
+  if (err) throw err;
+
+  console.error('removed old docs');
+
+  // store an img in binary in mongo
+  var cat = new Cat;
+  cat.img.data = fs.readFileSync(imgPath);
+  cat.img.contentType = 'image/png';
+  cat.save(function (err, cat) {
+    if (err) throw err;
+
+    console.error('saved img to mongo');
+
+     // start a demo server
+     var server = express.createServer();
+     server.get('/', function (req, res, next) {
+       Cat.findById(cat, function (err, doc) {
+         if (err) return next(err);
+         res.contentType(doc.img.contentType);
+         res.send(doc.img.data);
+       });
+     });
+
+     server.on('close', function () {
+      console.error('dropping db');
+      mongoose.connection.db.dropDatabase(function () {
+        console.error('closing db connection');
+        mongoose.connection.close();
+      });
+    });
+
+    server.listen(3333, function (err) {
+      var address = server.address();
+      console.error('server listening on http://%s:%d', address.address, address.port);
+      console.error('press CTRL+C to exit');
+    });
+
+    process.on('SIGINT', function () {
+      server.close();
+    });
   });
-
 });
 
-module.exports = File;
+});
--- src/server.js
+++ src/server.js
@@ -8,15 +8,18 @@ const app = express();
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 const config = require('./config');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const router = require('./routes');
 
-// mongoose.connection.openUri(`mongodb://${config.db.username}:${config.db.password}@${config.db.host}/${config.db.dbName}`);
+app.use('/api', router);
+
+mongoose.connection.openUri(`mongodb://${config.db.username}:${config.db.password}@${config.db.host}/${config.db.dbName}`);
 
 // Import all models
 // require('./models/file.model.js');
 
-// app.listen(4949, function() {
-//   console.log(`SuperApp is listening on port ${4949}`);
-// });
+
 
 app.listen(config.port, function() {
   console.log(`${config.appName} is listening on port ${config.port}`);
