41e3983854d4ffc2774a82060d63b802cb02d822
Unknown <oreoboi@gmail.com>
2018-03-29 16:48:17 -0400

    comment out

--- src/models/file.models.js
+++ src/models/file.models.js
@@ -5,7 +5,7 @@ var express = require('express');
 var Schema = mongoose.Schema;
 
 //img path
-var imgPath = '../../public/pics/cat1.jpg';
+var imgPath = 'public/pics/cat1.jpg';
 
 // Image schema
 const schema = new mongoose.Schema({
@@ -32,39 +32,39 @@ Cat.remove(function (err) {
   var cat = new Cat;
   cat.img.data = fs.readFileSync(imgPath);
   cat.img.contentType = 'image/png';
-  cat.save(function (err, cat) {
-    if (err) throw err;
+  // cat.save(function (err, cat) {
+  //   if (err) throw err;
 
-    console.error('saved img to mongo');
+  //   console.error('saved img to mongo');
 
-     // start a demo server
-     var server = express.createServer();
-     server.get('/', function (req, res, next) {
-       Cat.findById(cat, function (err, doc) {
-         if (err) return next(err);
-         res.contentType(doc.img.contentType);
-         res.send(doc.img.data);
-       });
-     });
+  //    // start a demo server
+  //    var server = express.createServer();
+  //    server.get('/', function (req, res, next) {
+  //      Cat.findById(cat, function (err, doc) {
+  //        if (err) return next(err);
+  //        res.contentType(doc.img.contentType);
+  //        res.send(doc.img.data);
+  //      });
+  //    });
 
-     server.on('close', function () {
-      console.error('dropping db');
-      mongoose.connection.db.dropDatabase(function () {
-        console.error('closing db connection');
-        mongoose.connection.close();
-      });
-    });
+  //    server.on('close', function () {
+  //     console.error('dropping db');
+  //     mongoose.connection.db.dropDatabase(function () {
+  //       console.error('closing db connection');
+  //       mongoose.connection.close();
+  //     });
+  //   });
 
-    server.listen(3333, function (err) {
-      var address = server.address();
-      console.error('server listening on http://%s:%d', address.address, address.port);
-      console.error('press CTRL+C to exit');
-    });
+  //   server.listen(3333, function (err) {
+  //     var address = server.address();
+  //     console.error('server listening on http://%s:%d', address.address, address.port);
+  //     console.error('press CTRL+C to exit');
+  //   });
 
-    process.on('SIGINT', function () {
-      server.close();
-    });
-  });
+  //   process.on('SIGINT', function () {
+  //     server.close();
+  //   });
+  // });
 });
 
 });
